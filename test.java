cd87c60a65c336177290a1f33e4f77075002a38c
Nadia Tarashkevich <nadia.tarashkevich@jetbrains.com>
2024-10-25 21:25:17 -1000

    Added java test file

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+    public void func(int a, int b, int c){
+        System.out.println(a+b+c);
+    }
+}
